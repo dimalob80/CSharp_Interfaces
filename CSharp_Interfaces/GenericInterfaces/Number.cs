e5a0ebec5614de4b11c0e9e3a919ff19298e4f53
Dmitry Lobanov <DmitryL@iqmetrix.com>
2022-06-13 10:33:05 -0500

    Generic Interfaces

---
+++ CSharp_Interfaces/GenericInterfaces/Number.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GenericInterfaces
+{
+    internal class Number : IComparable<int>, IComparable<string>
+    {
+        public int Value { get; init; }
+
+        public int CompareTo(int other)  =>
+            Value.CompareTo(other);
+
+        public int CompareTo(string? other)  =>
+            Value.CompareTo(int.Parse(other!)); 
+    }
+}
---
+++ CSharp_Interfaces/GenericInterfaces/Program.cs
@@ -0,0 +1,60 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using GenericInterfaces;
+
+//WithBoxing();
+//WithoutBoxing();
+
+var number = new Number() { Value = 10 };
+
+//using overloaded methods of the interface
+Console.WriteLine(number.CompareTo(10)); 
+Console.WriteLine(number.CompareTo("10"));
+
+//using certian type of the Generic interface
+IComparable<int> comparableInt = number;
+comparableInt.CompareTo(10);
+
+IComparable<string> comparableString = number;
+comparableString.CompareTo("10");
+
+
+//---------------
+static void WithBoxing()
+{
+    var x = 1;
+    var y = 2;
+
+    //Non-generic interface
+    IComparable comparable = x;
+
+    // Boxing:
+    // IComparable.CompareTo(object obj) - value type int will be packed into reference type
+    comparable.CompareTo(y);
+
+    // Runtime error: 'Object must be of type Int32.'
+    // Code is legal as CompareTo excpects an object, but it will throw exception as it will try to compare int and string, which is impossible without parsing; 
+    comparable.CompareTo("2");
+
+    Console.WriteLine("Boxing");
+}
+
+static void WithoutBoxing()
+{
+    var x = 1;
+    var y = 2;
+
+    //Generic interface with type
+    IComparable<int> comparable = x;
+
+    // No Boxing:
+    // IComparable.CompareTo(object obj) - value type int will be packed into reference type
+    comparable.CompareTo(y);
+
+    // Compile error:
+    // comparable.CompareTo("2"); // CS1503: Argument 1: cannot convert from string to int.
+
+    Console.WriteLine("Without boxing");
+}
+
+//-------------------------------------
